3638be408eb31a798546c279b1ede1c2e8f29d79
sajjad ramezani <sajjadramezani780@gmail.com>
2022-05-15 15:11:55 +0430

    2nd commit

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React,{useState} from "react";
+import React,{useState,useEffect} from "react";
 import "./App.css";
 import { Card } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
@@ -6,6 +6,7 @@ import Todo from "./comonents/Todo";
 import FormTodo from "./comonents/FromTodo";
 import FormTodo1 from "./comonents/FromTodo1";
 import FormTodo2 from "./comonents/FromTodo2";
+import axios from "axios";
 
 
 function App() {
@@ -27,20 +28,40 @@ function App() {
       isDone: false
     }
   ]);
-  
+  // useEffect(()=>{
+  //   axios.post("https://gitlab.com/api/v4/projects/36163448/issues?access_token=glpat-KK318oz33CgxyjbTWnf5",{title:"hi",lables:"hello"})
+  //   .then(function (response) {
+  //     console.log(response);
+  //   })
+  //   .catch(function (error) {
+  //     console.log(error);
+  //   });},[])
+
 
 
   const addTodo = text => {
     const newTodos = [...todos, { text }];
     setTodos(newTodos);
+    axios.post("https://gitlab.com/api/v4/projects/36163448/issues?access_token=glpat-KK318oz33CgxyjbTWnf5",{title:`${text}`,labels:"bug"})
+    .then(function(response){
+      console.log(response)
+    })
   };
   const addTodo1 = text => {
     const newTodos1 = [...todos1, { text }];
     setTodos1(newTodos1);
+    axios.post("https://gitlab.com/api/v4/projects/36163448/issues?access_token=glpat-KK318oz33CgxyjbTWnf5",{title:`${text}`,labels:"question"})
+    .then(function(response){
+      console.log(response)
+    })
   };
   const addTodo2 = text => {
     const newTodos2 = [...todos2, { text }];
     setTodos2(newTodos2);
+    axios.post("https://gitlab.com/api/v4/projects/36163448/issues?access_token=glpat-KK318oz33CgxyjbTWnf5",{title:`${text}`,labels:"enhancement"})
+    .then(function(response){
+      console.log(response)
+    })
   };
   
 
@@ -83,7 +104,7 @@ function App() {
      <div className="row justify-content-between">
        
       <div className="col">
-        <h6>To Do </h6>
+        <h6 id="demo">To Do </h6>
         <FormTodo addTodo={addTodo} />
           {todos.map((todo, index) => (
             <Card>
